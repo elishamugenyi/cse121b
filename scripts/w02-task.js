8861dc765354112d986b98f62478d8ca93eb536a
elishamugenyi <elishamugenyi5@gmail.com>
2024-01-15 22:24:57 +0300

    added js

---
+++ scripts/w02-task.js
@@ -0,0 +1,37 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+
+//declared and instastiate a fullname variable
+const fullname = "Elisha Mugenyi";
+
+//create variable for current year
+let currentYear = new Date().getFullYear();
+
+//create variable for image path
+let profilePicture = "images/el.jpg";
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('#img');
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullname}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute('scr', profilePicture);
+imageElement.setAttribute('alt',`Profile image of ${fullName}`);
+
+
+
+
+/* Step 5 - Array */
+
+
+
+
+
+
