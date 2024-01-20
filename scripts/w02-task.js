e7f7be8d5ee0c02ca2cf7312e2c587e64cec8ad4
elishamugenyi <elishamugenyi5@gmail.com>
2024-01-21 00:18:30 +0300

    corrected img id

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -17,7 +17,7 @@ let profilePicture = "images/el.jpg";
 const nameElement = document.getElementById("name");
 const foodElement = document.getElementById('food');
 const yearElement = document.querySelector('#year');
-const imageElement = document.querySelector('#img');
+const imageElement = document.getElementById("img");
 
 /* Step 4 - Adding Content */
 nameElement.innerHTML = `<strong>${fullname}</strong>`;
