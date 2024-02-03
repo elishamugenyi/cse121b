012721fb1015d94665c4ce4f0ec38803887e61be
elishamugenyi <elishamugenyi5@gmail.com>
2024-02-03 15:25:41 +0300

    created an empty object and added attributes.

---
+++ scripts/w04-task.js
@@ -0,0 +1,50 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+let myProfile = {
+    name: "Elisha Mugenyi",
+    photo: "images/el.jpg",
+    favoriteFoods: ['Rice','Pudding','chapati','pork'],
+    hobbies: ['Gyming','Football','Programming'],
+    placesLived: []
+};
+
+// Add the newPlace object to the placesLived array
+myProfile.placesLived.push(
+    {
+        place: "Wakiso, kajjansi",
+        length: "20 years"
+    },
+    {
+        place: "Lweza",
+        length: "3 years"
+    },
+    {
+        place: "Ntinda",
+        length: "5 years"
+    }
+);
+
+
+
+/* Populate Profile Object with placesLive objects */
+
+
+
+
+/* DOM Manipulation - Output */
+
+/* Name */
+
+/* Photo with attributes */
+
+
+/* Favorite Foods List*/
+
+
+/* Hobbies List */
+
+
+/* Places Lived DataList */
+
+
