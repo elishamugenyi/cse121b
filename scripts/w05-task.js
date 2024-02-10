c241f6b571802f0fd8c334bb8c2ce995840d383c
elishamugenyi <elishamugenyi5@gmail.com>
2024-02-10 21:01:21 +0300

    changed the url for temples

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -25,7 +25,7 @@ const displayTemples = (temples) => {
 
 /* async getTemples Function using fetch()*/
 const getTemples = async () => {
-    const response = await fetch('temples.json');
+    const response = await fetch('https://byui-cse.github.io/cse121b-ww-course/resources/temples.json');
     templeList = await response.json();
     displayTemples(templeList);
   };
